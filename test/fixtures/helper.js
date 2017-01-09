ffc345ee5a845369529d7b0edded35cd70cd97df
James Messinger <james.messinger@gmail.com>
2017-01-09 07:28:37 -0600

    fixed a bug where all console output was suppressed (even Mocha's output)

--- test/fixtures/helper.js
+++ test/fixtures/helper.js
@@ -5,14 +5,16 @@ const sinon = require('sinon');
 const path = require('path');
 const childProcess = require('child_process');
 
+const originalLogMethod = console.log;
+
 module.exports = { assert };
 
 // Run all the tests from the "sample-package" directory
 process.chdir(path.join(__dirname, '../', 'sample-package'));
 
 beforeEach(() => {
-  sinon.stub(console, 'log');
   sinon.stub(childProcess, 'fork');
+  sinon.stub(console, 'log', captureLogs);
 });
 
 afterEach(() => {
@@ -43,3 +45,14 @@ function assert (cmd, calls) {
   });
 }
 
+/**
+ * Suppresses certain console logs, to avoid cluttering the test results
+ */
+function captureLogs () {
+  if (arguments[0] === 'browserify' || arguments[0] === 'watchify') {
+    // suppress this output, so we don't clutter the test results
+  }
+  else {
+    originalLogMethod.apply(console, arguments);
+  }
+}
