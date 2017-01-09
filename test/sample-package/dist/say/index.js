38c2d75ec921228173542d1364307f4da1f62655
James Messinger <james.messinger@gmail.com>
2017-01-09 06:27:01 -0600

    renamed the "tests" directory to "test", which is what Mocha expects by default

---
+++ test/sample-package/dist/say/index.js
@@ -0,0 +1,14 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+/**
+ * Says something to somebody.
+ * @param {string} what - What to say
+ * @param {string} [who] - Who to say goodbye to
+ */
+module.exports = function say(what, who) {
+  console.log('%s, %s!', what, who);
+};
+
+
+},{}]},{},[1]);
